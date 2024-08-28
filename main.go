44e69114231cfe907f276d62533f4fa6c298e2a8
Dafa Ardiansyah <ardidafa21@gmail.com>
2024-08-28 14:56:34 +0700

    Add Api GET Method /campaigns/:id in main.go

--- main.go
+++ main.go
@@ -45,6 +45,7 @@ func main() {
 	api.POST("/avatars", authMiddleware(authService, userService), userHandler.UploadAvatar) //tested -middleware
 
 	api.GET("/campaigns", campaignHandler.GetCampaigns)
+	api.GET("/campaigns/:id", campaignHandler.GetCampaign)
 
 	router.Run()
 }
