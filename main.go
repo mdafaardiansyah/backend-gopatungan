f9ed3913a2268ed56b6916f418cee5f45122fade
Dafa Ardiansyah <ardidafa21@gmail.com>
2024-08-28 12:56:04 +0700

    Add static router for images

--- main.go
+++ main.go
@@ -35,6 +35,8 @@ func main() {
 	campaignHandler := handler.NewCampaignHandler(campaignService)
 
 	router := gin.Default()
+	router.Static("/images", "./images")
+
 	api := router.Group("/api/v1")
 
 	api.POST("/users", userHandler.RegisterUser)                                             //tested
