38b4c2b6eb6f5626c0f43789a8121389965c8bf3
Dafa Ardiansyah <ardidafa21@gmail.com>
2024-08-28 11:53:47 +0700

    Rename FindCampaign to GetCampaign

--- handler/campaign.go
+++ handler/campaign.go
@@ -1,1 +1,31 @@
 package handler
+
+import (
+	"Gopatungan/campaign"
+	"Gopatungan/helper"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+)
+
+type CampaignHandler struct {
+	service campaign.Service
+}
+
+func NewCampaignHandler(service campaign.Service) *CampaignHandler {
+	return &CampaignHandler{service}
+}
+
+func (h *CampaignHandler) GetCampaigns(c *gin.Context) {
+	userID, _ := strconv.Atoi(c.Query("user_id"))
+
+	campaigns, err := h.service.GetCampaigns(userID)
+	if err != nil {
+		response := helper.APIResponse("Error to get campaigns", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helper.APIResponse("List of Campaigns", http.StatusOK, "success", campaigns)
+	c.JSON(http.StatusOK, response)
+}
