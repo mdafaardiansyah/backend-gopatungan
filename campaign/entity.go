80bf217cebac43dca84e2ee905c1794ef026e5a7
Dafa Ardiansyah <ardidafa21@gmail.com>
2024-08-28 12:13:46 +0700

    Change UINT to INT for ID and UserID

--- campaign/entity.go
+++ campaign/entity.go
@@ -3,8 +3,8 @@ package campaign
 import "time"
 
 type Campaign struct {
-	ID               uint      `gorm:"primary_key" json:"id"`
-	UserID           uint      `gorm:"column:user_id" json:"user_id"`
+	ID               int       `gorm:"primary_key" json:"id"`
+	UserID           int       `gorm:"column:user_id" json:"user_id"`
 	Name             string    `gorm:"column:name" json:"name"`
 	ShortDescription string    `gorm:"column:short_description" json:"short_description"`
 	Description      string    `gorm:"column:long_description" json:"long_description"`
@@ -19,8 +19,8 @@ type Campaign struct {
 }
 
 type CampaignImage struct {
-	ID         uint      `gorm:"primary_key" json:"id"`
-	CampaignID uint      `gorm:"column:campaign_id" json:"campaign_id"`
+	ID         int       `gorm:"primary_key" json:"id"`
+	CampaignID int       `gorm:"column:campaign_id" json:"campaign_id"`
 	FileName   string    `gorm:"column:filename" json:"filename"`
 	IsPrimary  int       `gorm:"column:is_primary" json:"is_primary"`
 	CreatedAt  time.Time `gorm:"column:created_time;autoCreateTime" json:"created_time"`
