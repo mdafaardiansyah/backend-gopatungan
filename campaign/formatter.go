6c85ca9d49eb86e7a6c8882ad141d291ea609609
Dafa Ardiansyah <ardidafa21@gmail.com>
2024-08-28 12:49:34 +0700

    Make Simpler for anti null to blank Array in FormatCampaigns function

--- campaign/formatter.go
+++ campaign/formatter.go
@@ -29,11 +29,7 @@ func FormatCampaign(campaign Campaign) CampaignFormatter {
 }
 
 func FormatCampaigns(campaigns []Campaign) []CampaignFormatter {
-	if len(campaigns) == 0 {
-		return []CampaignFormatter{}
-	}
-
-	var campaignsFormatter []CampaignFormatter
+	campaignsFormatter := []CampaignFormatter{}
 
 	for _, campaign := range campaigns {
 		campaignFormatter := FormatCampaign(campaign)
