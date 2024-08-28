2aaa6561d262068cf1f58bd9983070e08d916e44
Dafa Ardiansyah <ardidafa21@gmail.com>
2024-08-28 20:11:40 +0700

    Add CreateCampaign() Function from input argument in Service for Campaign

--- campaign/service.go
+++ campaign/service.go
@@ -3,6 +3,7 @@ package campaign
 type Service interface {
 	GetCampaigns(userID int) ([]Campaign, error)
 	GetCampaignByID(input GetCampaignDetailInput) (Campaign, error)
+	CreateCampaign(input CreateCampaignInput) (Campaign, error)
 }
 
 type service struct {
@@ -35,4 +36,21 @@ func (s *service) GetCampaignByID(input GetCampaignDetailInput) (Campaign, error
 		return campaign, err
 	}
 	return campaign, nil
+}
+
+func (s *service) CreateCampaign(input CreateCampaignInput) (Campaign, error) {
+	campaign := Campaign{}
+
+	campaign.Name = input.Name
+	campaign.ShortDescription = input.ShortDesc
+	campaign.Description = input.Description
+	campaign.Perks = input.Perks
+	campaign.GoalAmount = input.GoalAmount
+	campaign.UserID = int(input.User.ID)
+
+	newCampaign, err := s.repository.Save(campaign)
+	if err != nil {
+		return newCampaign, err
+	}
+	return newCampaign, nil
 }
