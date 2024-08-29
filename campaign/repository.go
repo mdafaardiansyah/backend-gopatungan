49b77e0916f1981d4fadd2e12a58c00fcda37f05
Dafa Ardiansyah <ardidafa21@gmail.com>
2024-08-29 13:29:58 +0700

    Add Update Contract for Campaign/Repository

--- campaign/repository.go
+++ campaign/repository.go
@@ -7,6 +7,7 @@ type Repository interface {
 	FindByUserID(userID int) ([]Campaign, error)
 	FindByID(ID int) (Campaign, error)
 	Save(campaign Campaign) (Campaign, error)
+	Update(campaign Campaign) (Campaign, error)
 }
 
 type repository struct {
@@ -38,8 +39,8 @@ func (r *repository) FindByUserID(userID int) ([]Campaign, error) {
 }
 
 func (r *repository) FindByID(ID int) (Campaign, error) {
-
 	var campaign Campaign
+
 	err := r.db.Preload("User").Preload("CampaignImages").Where("id = ?", ID).Find(&campaign).Error
 	if err != nil {
 		return campaign, err
@@ -49,6 +50,14 @@ func (r *repository) FindByID(ID int) (Campaign, error) {
 
 func (r *repository) Save(campaign Campaign) (Campaign, error) {
 	err := r.db.Create(&campaign).Error
+	if err != nil {
+		return campaign, err
+	}
+	return campaign, nil
+}
+
+func (r *repository) Update(campaign Campaign) (Campaign, error) {
+	err := r.db.Save(&campaign).Error
 	if err != nil {
 		return campaign, err
 	}
