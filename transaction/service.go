b9c9185aa3627cf5c6b0ed402494174cbe0047d0
Dafa Ardiansyah <ardidafa21@gmail.com>
2024-09-02 11:10:11 +0700

    Implement GetTransactionsByCampaignID service method
    Added GetTransactionsByCampaignID method to retrieve transactions by campaign ID.
    Updated repository dependency to include GetByCampaignID method.

--- transaction/service.go
+++ transaction/service.go
@@ -1,1 +1,21 @@
 package transaction
+
+type service struct {
+	repository Repository
+}
+
+type Service interface {
+	GetTransactionsByCampaignID(campaignID int) ([]Transaction, error)
+}
+
+func NewService(repository Repository) *service {
+	return &service{repository}
+}
+
+func (s *service) GetTransactionsByCampaignID(campaignID int) ([]Transaction, error) {
+	transactions, err := s.repository.GetByCampaignID(campaignID)
+	if err != nil {
+		return transactions, err
+	}
+	return transactions, nil
+}
