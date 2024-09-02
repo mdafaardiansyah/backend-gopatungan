5ea3fd4ef9bc67c9c647ede5bdd5c51ef2447471
Dafa Ardiansyah <ardidafa21@gmail.com>
2024-09-02 11:28:08 +0700

    Implement GetCampaignTransaction handler method
    Added GetCampaignTransaction method to retrieve campaign transactions.
    Handles URI binding and error handling for transaction retrieval.

--- handler/transaction.go
+++ handler/transaction.go
@@ -1,1 +1,36 @@
 package handler
+
+import (
+	"Gopatungan/helper"
+	"Gopatungan/transaction"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type transactionHandler struct {
+	service transaction.Service
+}
+
+func NewTransactionHandler(service transaction.Service) *transactionHandler {
+	return &transactionHandler{service}
+}
+
+func (h *transactionHandler) GetCampaignTransaction(c *gin.Context) {
+	var input transaction.GetCampaignTransactionsInput
+
+	err := c.ShouldBindUri(&input)
+	if err != nil {
+		response := helper.APIResponse("Failed get campaign's transaction", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	transactions, err := h.service.GetTransactionsByCampaignID(input)
+	if err != nil {
+		response := helper.APIResponse("Failed get campaign's transaction", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+	response := helper.APIResponse("Campaign's transaction detail", http.StatusOK, "success", transactions)
+	c.JSON(http.StatusOK, response)
+}
