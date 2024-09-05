ff78f0421fd7c8bcb96da3d0a1e7b7c789f0cbb3
Dafa Ardiansyah <ardidafa21@gmail.com>
2024-09-05 08:30:57 +0700

    Feat : Save Contract & Implementation in repository.go transaction

--- transaction/repository.go
+++ transaction/repository.go
@@ -9,6 +9,7 @@ type repository struct {
 type Repository interface {
 	GetByCampaignID(campaignID int) ([]Transaction, error)
 	GetByUserID(userID int) ([]Transaction, error)
+	Save(transaction Transaction) (Transaction, error)
 }
 
 func NewRepository(db *gorm.DB) *repository {
@@ -32,4 +33,12 @@ func (r *repository) GetByUserID(userID int) ([]Transaction, error) {
 		return transactions, err
 	}
 	return transactions, nil
+}
+
+func (r *repository) Save(transaction Transaction) (Transaction, error) {
+	err := r.db.Save(&transaction).Error
+	if err != nil {
+		return transaction, err
+	}
+	return transaction, nil
 }
