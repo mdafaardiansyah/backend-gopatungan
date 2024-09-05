e3043aeebe763b910ec0eaeceae0ecf9d7600563
Dafa Ardiansyah <ardidafa21@gmail.com>
2024-09-05 09:13:41 +0700

    Feat : Add Handler for CreateTransaction() in handler\transaction.go

--- handler/transaction.go
+++ handler/transaction.go
@@ -52,5 +52,31 @@ func (h *transactionHandler) GetUserTransactions(c *gin.Context) {
 
 	response := helper.APIResponse("User's transaction detail", http.StatusOK, "success", transaction.FormatUserTransactions(transactions))
 	c.JSON(http.StatusOK, response)
+}
 
+func (h *transactionHandler) CreateTransaction(c *gin.Context) {
+	var input transaction.CreateTransactionInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.APIResponse("Failed create transaction", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	currentUser := c.MustGet("currentUser").(user.User)
+	input.User = currentUser
+
+	newTransaction, err := h.service.CreateTransaction(input)
+	if err != nil {
+		response := helper.APIResponse("Failed create transaction", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helper.APIResponse("Success create transaction", http.StatusOK, "success", newTransaction)
+	c.JSON(http.StatusOK, response)
 }
