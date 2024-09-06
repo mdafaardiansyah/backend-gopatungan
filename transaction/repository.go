627d5d833c2d89baeb9db7061c0992487d2034ee
Dafa Ardiansyah <ardidafa21@gmail.com>
2024-09-06 16:20:21 +0700

    Fix : r.db.Save(&transaction).Error to r.db.Create(&transaction).Error

--- transaction/repository.go
+++ transaction/repository.go
@@ -37,7 +37,7 @@ func (r *repository) GetByUserID(userID int) ([]Transaction, error) {
 }
 
 func (r *repository) Save(transaction Transaction) (Transaction, error) {
-	err := r.db.Save(&transaction).Error
+	err := r.db.Create(&transaction).Error
 	if err != nil {
 		return transaction, err
 	}
