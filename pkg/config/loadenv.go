d7dbeace4ce7a23bd83ead1bef6c67badbdf8a3e
Dafa Ardiansyah <ardidafa21@gmail.com>
2024-09-19 15:31:28 +0700

    CI/CD Fix : add error handlers ni loadenv.go

--- pkg/config/loadenv.go
+++ pkg/config/loadenv.go
@@ -2,8 +2,13 @@ package config
 
 import (
 	"github.com/joho/godotenv"
+	"log"
 )
 
 func LoadEnv() error {
-	return godotenv.Load("/app/.env")
+	err := godotenv.Load("/app/.env")
+	if err != nil {
+		log.Fatal("Error loading .env file:", err) // Log a fatal error if .env loading fails
+	}
+	return nil
 }
