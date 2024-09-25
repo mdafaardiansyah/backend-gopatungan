4cae6e97ef095ad542deea64e861db897c0330cf
Dafa Ardiansyah <ardidafa21@gmail.com>
2024-09-25 11:11:58 +0700

    Fix Bug : Change to *

--- cmd/gopatungan/main.go
+++ cmd/gopatungan/main.go
@@ -134,7 +134,7 @@ func authMiddleware(authService auth.Service, userService user2.Service) gin.Han
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		ctx.Writer.Header().Set("Access-Control-Allow-Origin", ctx.Request.Header.Get("Origin"))
+		ctx.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		ctx.Writer.Header().Set("Access-Control-Max-Age", "86400")
 		ctx.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
 		ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, api_key, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
