d1153c7c524e907e0847f962952ee9b50c9e7ce1
Dafa Ardiansyah <ardidafa21@gmail.com>
2024-09-25 11:30:06 +0700

    Fix Bug : Fix CORS #4

--- cmd/gopatungan/main.go
+++ cmd/gopatungan/main.go
@@ -135,19 +135,15 @@ func authMiddleware(authService auth.Service, userService user2.Service) gin.Han
 func CORSMiddleware() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		ctx.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		ctx.Writer.Header().Set("Access-Control-Max-Age", "86400")
-		ctx.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
-		ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, api_key, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-		ctx.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length")
 		ctx.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		ctx.Writer.Header().Set("Cache-Control", "no-cache")
-		ctx.Writer.Header().Set("Vary", "Origin") // Add this line
+		ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		ctx.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
 
 		if ctx.Request.Method == "OPTIONS" {
-			log.Println("OPTIONS")
-			ctx.AbortWithStatus(200)
-		} else {
-			ctx.Next()
+			ctx.AbortWithStatus(204)
+			return
 		}
+
+		ctx.Next()
 	}
 }
